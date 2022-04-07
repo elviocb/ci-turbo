2bc27f1304766cc0a836e0720e4da600d70a377c
Elvio Cavalcante <elvio.cavalcante@toptal.com>
2022-04-07 11:04:10 -0300

    feat(docs): clean mess on docs

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,15 +3,7 @@ import {Button} from "ui"
 export default function Docs() {
   return (
     <div>
-      <h1>Docs XXXXX</h1>
-      <h1>Docs XXXXX</h1>
-      <h1>Docs XXXXX</h1>
-      <h1>Docs XXXXX</h1>
-      <h1>Docs XXXXX</h1>
-      <h1>Changes separated Commit DOCS</h1>
-      <h1>Changes separated Commit DOCS</h1>
-      <h1>Changes separated Commit DOCS</h1>
-      <h1>WORKSSSS</h1>
+      <h1>Docs</h1>
       <Button />
     </div>
   )
