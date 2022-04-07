3e2b9ff696c912a65eee75643d376212d72cb1c6
Elvio Cavalcante <elvio.cavalcante@toptal.com>
2022-04-07 14:36:01 -0300

    feat: remove web headers

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,10 +4,6 @@ export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <h1>Web</h1>
-      <h1>Web</h1>
-      <h1>Web</h1>
-      <h1>Web</h1>
       <Button />
     </div>
   )
